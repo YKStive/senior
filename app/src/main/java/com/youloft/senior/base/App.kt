cb49d7d33c30b174433f585364306f6cdf1cd8d7
lunhongshao <shaolunhong@163.com>
2020-06-19 16:02:57 +0800

    新增查看大图，影集详情等

--- app/src/main/java/com/youloft/senior/base/App.kt
+++ app/src/main/java/com/youloft/senior/base/App.kt
@@ -1,6 +1,8 @@
 package com.youloft.senior.base
 
 import android.app.Application
+import com.umeng.commonsdk.UMConfigure
+import com.umeng.socialize.PlatformConfig
 import kotlin.properties.Delegates
 
 /**
@@ -10,8 +12,17 @@ import kotlin.properties.Delegates
  */
 class App : Application() {
 
+    val context by lazy { this }
     override fun onCreate() {
         super.onCreate()
+        //初始化友盟分享
+        //初始化友盟分享
+        UMConfigure.init(this, "kasjdlkjslakj", "", UMConfigure.DEVICE_TYPE_PHONE, "")
+        UMConfigure.setLogEnabled(true)
+        PlatformConfig.setWeixin(
+            "wx5f3a0d4653cd3485",
+            "af16a3c2d7b39dd4e8022e04ca1baa3f"
+        )
 
     }
 
---
+++ app/src/main/java/com/youloft/senior/ui/detail/VideoDetailActivity.kt
@@ -0,0 +1,63 @@
+package com.youloft.senior.ui.detail
+
+import android.content.Intent
+import android.widget.Toast
+import com.youloft.core.base.BaseActivity
+import com.youloft.senior.R
+import com.youloft.socialize.SOC_MEDIA
+import com.youloft.socialize.Socialize
+import com.youloft.socialize.share.AbstractShareAction
+import com.youloft.socialize.share.ShareImage
+import com.youloft.socialize.share.ShareWeb
+import com.youloft.socialize.share.UmengShareActionImpl
+import kotlinx.android.synthetic.main.activity_video_detail.*
+
+/**
+ *
+ * @Description:     影集详情
+ * @Author:         slh
+ * @CreateDate:     2020/6/18 15:19
+ * @UpdateUser:     更新者：
+ * @UpdateDate:     2020/6/18 15:19
+ * @UpdateRemark:   更新说明：
+ * @Version:        1.0
+ */
+class VideoDetailActivity : BaseActivity() {
+    override fun getLayoutResId(): Int = R.layout.activity_video_detail
+
+    override fun initView() {
+        btn_share_com.setOnClickListener {
+            Toast.makeText(this@VideoDetailActivity, "点击", Toast.LENGTH_SHORT).show()
+            UmengShareActionImpl(this@VideoDetailActivity).platform(SOC_MEDIA.WEIXIN_CIRCLE).web(
+                ShareWeb("www.baidu.com").setThumb(ShareImage(this@VideoDetailActivity, ""))
+                    .setDescription("内容").setTitle("标题")).perform()
+
+
+//                .prepare(object :
+//                    AbstractShareAction.PrepareListener {
+//                    override fun onWorkThread(): Any {
+//                        return Any();
+//                    }
+//
+//                    override fun onMainThread(value: Any?) {
+//                    }
+//
+//                })
+//            )
+//           var board= Socialize.getIns().newShareBoard(this@VideoDetailActivity);
+//            board.withPlatform(SOC_MEDIA.WEIXIN).web(
+//                ShareWeb("www.baidu.com").setThumb(ShareImage(this@VideoDetailActivity, ""))
+//                    .setDescription("内容").setTitle("标题")
+//            )
+//            board.shareWithUI()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun initData() {
+    }
+}
+
