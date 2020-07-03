0e7304ae9cf1efb2e163b6132d938ab7443ddcdd
xll <237372185@qq.com>
2020-07-03 14:15:31 +0800

    点击态处理

---
+++ app/src/main/java/com/youloft/senior/widgt/PressLinearLayout.kt
@@ -0,0 +1,33 @@
+package com.youloft.senior.widgt
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.widget.LinearLayout
+import androidx.appcompat.widget.AppCompatTextView
+
+/**
+ * @author xll
+ * @date 2020/7/3 9:33
+ */
+class PressLinearLayout(
+    context: Context?,
+    attrs: AttributeSet?
+) : LinearLayout(context, attrs) {
+    private val helper = PressHelper(this)
+
+    override fun dispatchSetPressed(pressed: Boolean) {
+        super.dispatchSetPressed(pressed)
+        helper.dispatchSetPressed(pressed)
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        helper.onSizeChanged(w, h, oldw, oldh)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        helper.onDraw(canvas)
+        super.onDraw(canvas)
+    }
+}
